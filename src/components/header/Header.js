634ed067a9af7145233553107a17378063df0345
Feng Liang <feng.liang@kaust.edu.sa>
2022-03-02 17:54:56 +0300

    fix header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -70,7 +70,7 @@ class Header extends Component {
                   onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
                   onMouseOut={(event) => onMouseOut(event)}
                 >
-                  Experience
+                  Careers
                 </NavLink>
               </li>
               <li>
